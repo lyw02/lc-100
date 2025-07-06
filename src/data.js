776639d4c316900aa6ff0b4329f98f805310ddc0
Yiwei Liu <liu02yiwei@gmail.com>
2025-07-06 17:23:01 +0100

    spiral-matrix

--- src/data.js
+++ src/data.js
@@ -164,6 +164,49 @@ n == matrix[0].length
 `,
     "link": "https://leetcode.cn/problems/set-matrix-zeroes/description/?envType=study-plan-v2&envId=top-100-liked"
   },
+  {
+    "id": 54,
+    "title": "螺旋矩阵 spiral-matrix",
+    "category": "矩阵",
+    "content": `
+给你一个 m 行 n 列的矩阵 matrix ，请按照 顺时针螺旋顺序 ，返回矩阵中的所有元素。
+
+
+
+示例 1：
+
+输入：matrix = [
+                [1,2,3],
+                [4,5,6],
+                [7,8,9]
+              ]
+输出：[1,2,3,6,9,8,7,4,5]
+
+示例 2：
+
+输入：matrix = [
+                [1,2,3,4],
+                [5,6,7,8],
+                [9,10,11,12]
+              ]
+输出：[1,2,3,4,8,12,11,10,9,5,6,7]
+
+
+提示：
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 10
+-100 <= matrix[i][j] <= 100
+    `,
+    "difficulty": "中等",
+    "hint": `
+- 使用 l, r, t, b 四个变量存储当前边界的位置
+- 按照螺旋顺序 l -> r, t -> b, r -> l, b -> t 不断遍历
+- 遍历到边界后改变对应边界变量的值（收缩边界）
+`,
+    "link": "https://leetcode.cn/problems/spiral-matrix/description/?envType=study-plan-v2&envId=top-100-liked"
+  },
 ]
 
 export default data;
