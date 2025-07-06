6af55f4d8c3317ab9c8cc5d4b9008e71f4abf891
Yiwei Liu <liu02yiwei@gmail.com>
2025-07-06 20:23:29 +0100

    rotate-image

--- src/data.js
+++ src/data.js
@@ -207,6 +207,66 @@ n == matrix[i].length
 `,
     "link": "https://leetcode.cn/problems/spiral-matrix/description/?envType=study-plan-v2&envId=top-100-liked"
   },
+  {
+    "id": 48,
+    "title": "旋转图像 rotate-image",
+    "category": "矩阵",
+    "content": `
+给定一个 n × n 的二维矩阵 matrix 表示一个图像。请你将图像顺时针旋转 90 度。
+
+你必须在 原地 旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要 使用另一个矩阵来旋转图像。
+
+ 
+示例 1：
+
+输入：matrix = [
+                [1,2,3],
+                [4,5,6],
+                [7,8,9]
+              ]
+输出：[
+        [7,4,1],
+        [8,5,2],
+        [9,6,3]
+      ]
+
+示例 2：
+
+输入：matrix = [
+                [5,1,9,11],
+                [2,4,8,10],
+                [13,3,6,7],
+                [15,14,12,16]
+              ]
+输出：[
+        [15,13,2,5],
+        [14,3,4,1],
+        [12,6,8,9],
+        [16,7,10,11]
+      ]
+
+
+提示：
+
+n == matrix.length == matrix[i].length
+1 <= n <= 20
+-1000 <= matrix[i][j] <= 1000
+    `,
+    "difficulty": "中等",
+    "hint": `
+思路一：常规
+    - 使用辅助数组 tmp 复制原数组
+    - matrix[j][n - i - 1] = tmp[i][j]
+    - 不符合要求
+思路二：先转置再每行倒序
+    - 原地求转置的方法：
+      - 如果是方阵：
+        - 对于 i < j 的元素，交换 matrix[i][j] 和 matrix[j][i]
+      - 如果不是方阵：
+        - 通过补 0 形成方阵
+`,
+    "link": "https://leetcode.cn/problems/rotate-image/description/?envType=study-plan-v2&envId=top-100-liked"
+  },
 ]
 
 export default data;
