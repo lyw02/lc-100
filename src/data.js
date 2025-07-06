5654e102bf16cb42e1b47c5586446a9a1b9cff20
Yiwei Liu <liu02yiwei@gmail.com>
2025-07-06 16:05:51 +0100

    product-of-array-except-self

--- src/data.js
+++ src/data.js
@@ -47,7 +47,53 @@ const data = [
     - 然后将翻转后的数组的[0, (k mod n) - 1], [k mod n, n - 1] 子数组分别翻转即可
 `,
     "link": "https://leetcode.cn/problems/rotate-array/description/?envType=study-plan-v2&envId=top-100-liked"
-  }
+  },
+  {
+    "id": 238,
+    "title": "除自身以外数组的乘积 product-of-array-except-self",
+    "category": "普通数组",
+    "content": `
+给你一个整数数组 nums，返回 数组 answer ，其中 answer[i] 等于 nums 中除 nums[i] 之外其余各元素的乘积 。
+
+题目数据 保证 数组 nums之中任意元素的全部前缀元素和后缀的乘积都在  32 位 整数范围内。
+
+请 不要使用除法，且在 O(n) 时间复杂度内完成此题。
+
+ 
+
+示例 1:
+
+输入: nums = [1,2,3,4]
+输出: [24,12,8,6]
+示例 2:
+
+输入: nums = [-1,1,0,-3,3]
+输出: [0,0,9,0,0]
+ 
+
+提示：
+
+2 <= nums.length <= 105
+-30 <= nums[i] <= 30
+输入 保证 数组 answer[i] 在  32 位 整数范围内
+ 
+
+进阶：你可以在 O(1) 的额外空间复杂度内完成这个题目吗？（ 出于对空间复杂度分析的目的，输出数组 不被视为 额外空间。）
+    `,
+    "difficulty": "中等",
+    "hint": `
+假如nums为[1,2,3,4]，那么answer的值分别为[(2,3,4),(1,3,4),(1,2,4),(1,2,3)]
+如果把i当前值相乘的时候看做是1那么就有如下样式
+ 1, 2, 3, 4
+ 1, 1, 3, 4
+ 1, 2, 1, 4
+ 1, 2, 3, 1
+他的对角线1将他们分割成了两个三角形，对于answer的元素，
+我们可以先计算一个三角形每行的乘积，然后再去计算另外一个三角形每行的乘积，
+然后各行相乘，就是answer每个对应的元素
+`,
+    "link": "https://leetcode.cn/problems/product-of-array-except-self/description/?envType=study-plan-v2&envId=top-100-liked"
+  },
 ]
 
 export default data;
