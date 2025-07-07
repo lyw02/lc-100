fe590049189c5177c8d55a69f0ec0c56ed024bf0
Yiwei Liu <liu02yiwei@gmail.com>
2025-07-07 22:34:23 +0100

    copy-list-with-random-pointer

--- src/data.js
+++ src/data.js
@@ -776,6 +776,65 @@ l1 和 l2 均按 非递减顺序 排列
     `,
     "link": "https://leetcode.cn/problems/swap-nodes-in-pairs/description/?envType=study-plan-v2&envId=top-100-liked"
   },
+  {
+    "id": 138,
+    "title": "随机链表的复制 copy-list-with-random-pointer",
+    "category": "链表",
+    "content": `
+给你一个长度为 n 的链表，每个节点包含一个额外增加的随机指针 random ，该指针可以指向链表中的任何节点或空节点。
+
+构造这个链表的 深拷贝。 深拷贝应该正好由 n 个 全新 节点组成，其中每个新节点的值都设为其对应的原节点的值。新节点的 next 指针和 random 指针也都应指向复制链表中的新节点，并使原链表和复制链表中的这些指针能够表示相同的链表状态。复制链表中的指针都不应指向原链表中的节点 。
+
+例如，如果原链表中有 X 和 Y 两个节点，其中 X.random --> Y 。那么在复制链表中对应的两个节点 x 和 y ，同样有 x.random --> y 。
+
+返回复制链表的头节点。
+
+用一个由 n 个节点组成的链表来表示输入/输出中的链表。每个节点用一个 [val, random_index] 表示：
+
+val：一个表示 Node.val 的整数。
+random_index：随机指针指向的节点索引（范围从 0 到 n-1）；如果不指向任何节点，则为  null 。
+你的代码 只 接受原链表的头节点 head 作为传入参数。
+
+
+
+示例 1：
+
+输入：head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
+输出：[[7,null],[13,0],[11,4],[10,2],[1,0]]
+
+示例 2：
+
+输入：head = [[1,1],[2,1]]
+输出：[[1,1],[2,1]]
+
+示例 3：
+
+输入：head = [[3,null],[3,0],[3,null]]
+输出：[[3,null],[3,0],[3,null]]
+ 
+
+提示：
+
+0 <= n <= 1000
+-104 <= Node.val <= 104
+Node.random 为 null 或指向链表中的节点。
+    `,
+    "difficulty": "中等",
+    "hint": `
+思路一：哈希表
+    - 遍历链表，使用哈希表记录每一个节点的拷贝，其中键为旧节点，值为新创建的节点，只设置 val
+    - 再次遍历，结合哈希表查找构建新节点的 next 和 random 指向
+思路二：拼接+拆分（理论空间更优，但实际更慢且空间开销更大）
+    - 1. 拼接
+      - 设原链表为 node1 -> node2 -> ...
+      - 构造新链表：node1 -> newNode1 -> node2 -> newNode2 -> ...
+      - 若节点 cur 的 random 指向 cur.random，那么对应的新节点 cur.next 的 random 指向 cur.random.next
+    - 2. 拆分
+      - 使用指针 pre 和 cur 分别指向两个链表的头节点
+      - 遍历执行 pre.next = pre.next.next 和 cur.next = cur.next.next 来拆分链表
+    `,
+    "link": "https://leetcode.cn/problems/copy-list-with-random-pointer/description/?envType=study-plan-v2&envId=top-100-liked"
+  },
 ]
 
 export default data;
