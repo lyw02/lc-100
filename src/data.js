3fcf408ab8f9943a624f8b26519b1cc9663b6448
Yiwei Liu <liu02yiwei@gmail.com>
2025-07-07 16:37:32 +0100

    merge-two-sorted-lists

--- src/data.js
+++ src/data.js
@@ -606,6 +606,44 @@ pos 的值为 -1 或者链表中的一个有效索引
 `,
     "link": "https://leetcode.cn/problems/linked-list-cycle-ii/description/?envType=study-plan-v2&envId=top-100-liked"
   },
+  {
+    "id": 21,
+    "title": "合并两个有序链表 merge-two-sorted-lists",
+    "category": "链表",
+    "content": `
+将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+
+ 
+
+示例 1：
+
+输入：l1 = [1,2,4], l2 = [1,3,4]
+输出：[1,1,2,3,4,4]
+
+示例 2：
+
+输入：l1 = [], l2 = []
+输出：[]
+
+示例 3：
+
+输入：l1 = [], l2 = [0]
+输出：[0]
+ 
+
+提示：
+
+两个链表的节点数目范围是 [0, 50]
+-100 <= Node.val <= 100
+l1 和 l2 均按 非递减顺序 排列
+    `,
+    "difficulty": "中等",
+    "hint": `
+- 使用 dummyHead 节点
+- 注意两链表长度不同的情况
+`,
+    "link": "https://leetcode.cn/problems/merge-two-sorted-lists/description/?envType=study-plan-v2&envId=top-100-liked"
+  },
 ]
 
 export default data;
