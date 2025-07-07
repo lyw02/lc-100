d98f8b50439ab6f4618aa92b4f3b9479d38ea33a
Yiwei Liu <liu02yiwei@gmail.com>
2025-07-07 17:00:57 +0100

    add-two-numbers

--- src/data.js
+++ src/data.js
@@ -637,12 +637,54 @@ pos 的值为 -1 或者链表中的一个有效索引
 -100 <= Node.val <= 100
 l1 和 l2 均按 非递减顺序 排列
     `,
-    "difficulty": "中等",
+    "difficulty": "简单",
     "hint": `
 - 使用 dummyHead 节点
 - 注意两链表长度不同的情况
 `,
     "link": "https://leetcode.cn/problems/merge-two-sorted-lists/description/?envType=study-plan-v2&envId=top-100-liked"
+  },
+  {
+    "id": 2,
+    "title": "两数相加 add-two-numbers",
+    "category": "链表",
+    "content": `
+给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+
+请你将两个数相加，并以相同形式返回一个表示和的链表。
+
+你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+
+ 
+
+示例 1：
+
+输入：l1 = [2,4,3], l2 = [5,6,4]
+输出：[7,0,8]
+解释：342 + 465 = 807.
+
+示例 2：
+
+输入：l1 = [0], l2 = [0]
+输出：[0]
+
+示例 3：
+
+输入：l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+输出：[8,9,9,9,0,0,0,1]
+ 
+
+提示：
+
+每个链表中的节点数在范围 [1, 100] 内
+0 <= Node.val <= 9
+题目数据保证列表表示的数字不含前导零
+    `,
+    "difficulty": "中等",
+    "hint": `
+- 依次处理，注意进位和长度不相等的情况
+`,
+    "link": "https://leetcode.cn/problems/add-two-numbers/description/?envType=study-plan-v2&envId=top-100-liked"
   },
 ]
 
