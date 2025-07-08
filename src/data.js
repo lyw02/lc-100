9ab6287fcd8ef0d6a86e09d43c0b725838ff69eb
Yiwei Liu <liu02yiwei@gmail.com>
2025-07-08 14:26:36 +0100

    sort-list

--- src/data.js
+++ src/data.js
@@ -835,6 +835,52 @@ Node.random 为 null 或指向链表中的节点。
     `,
     "link": "https://leetcode.cn/problems/copy-list-with-random-pointer/description/?envType=study-plan-v2&envId=top-100-liked"
   },
+  {
+    "id": 148,
+    "title": "排序链表 sort-list",
+    "category": "链表",
+    "content": `
+给你链表的头结点 head ，请将其按 升序 排列并返回 排序后的链表 。
+
+ 
+
+示例 1：
+
+输入：head = [4,2,1,3]
+输出：[1,2,3,4]
+
+示例 2：
+
+输入：head = [-1,5,3,4,0]
+输出：[-1,0,3,4,5]
+
+示例 3：
+
+输入：head = []
+输出：[]
+ 
+
+提示：
+
+链表中节点的数目在范围 [0, 5 * 104] 内
+-105 <= Node.val <= 105
+ 
+
+进阶：你可以在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序吗？
+    `,
+    "difficulty": "中等",
+    "hint": `
+- 题目要求时间空间复杂度分别为 O(nlogn) 和 O(1)，根据时间复杂度我们自然想到二分法，从而联想到归并排序
+- 基于递归的归并排序在递归调用时具有 O(logn) 的空间复杂度，因此不能使用递归
+- 因此考虑从底至顶直接合并：
+    - 归并排序本质上时通过二分法得到链表最小单元，再依次向上合并
+    - 每轮合并都有固定的操作长度 intv
+      - 第一轮 intv = 1，第二轮 intv = 2, 第三轮 intv = 4...
+      - 每轮结束后 intv *= 2，若 intv 大于数组长度则排序完成
+    - 使用一个 dummyHead 节点，既可以在每轮合并后找到头节点，也可以在排序时辅助交换指针
+    `,
+    "link": "https://leetcode.cn/problems/sort-list/description/?envType=study-plan-v2&envId=top-100-liked"
+  },
 ]
 
 export default data;
