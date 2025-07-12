a74b796352f0bad897f11b04fa16e2fe401745a7
Yiwei Liu <liu02yiwei@gmail.com>
2025-07-12 19:23:47 +0100

    binary-tree-level-order-traversal

--- src/data.js
+++ src/data.js
@@ -1149,6 +1149,51 @@ lRUCache.get(4);    // 返回 4
     `,
     link: "https://leetcode.cn/problems/diameter-of-binary-tree/description/?envType=study-plan-v2&envId=top-100-liked",
   },
+  {
+    id: 102,
+    title: "二叉树的层序遍历 binary-tree-level-order-traversal",
+    category: "二叉树",
+    content: `
+给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。
+
+
+示例 1：
+
+   3
+9    20
+   15   7
+
+输入：root = [3,9,20,null,null,15,7]
+输出：[[3],[9,20],[15,7]]
+
+示例 2：
+
+输入：root = [1]
+输出：[[1]]
+
+示例 3：
+
+输入：root = []
+输出：[]
+
+
+提示：
+
+树中节点数目在范围 [0, 2000] 内
+-1000 <= Node.val <= 1000
+    `,
+    difficulty: "中等",
+    hint: `
+思路一：迭代（BFS）
+    - 使用队列
+    - 首先将根节点入队，然后根据队列长度计算每层节点数，将当前层节点依次出队，添加到结果列表中，并将左右子节点入队
+思路二：递归（DFS）（更快）
+    - 在递归函数中使用 level 参数跟踪层级
+    - 如果 level 等于结果列表的长度，说明需要在结果列表中添加一个新的列表
+    - 递归左右子节点
+    `,
+    link: "https://leetcode.cn/problems/binary-tree-level-order-traversal/description/?envType=study-plan-v2&envId=top-100-liked",
+  },
 ];
 
 export default data;
