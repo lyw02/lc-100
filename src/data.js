39798e7610c296ec33b30efd9af3a2697a6694eb
Yiwei Liu <liu02yiwei@gmail.com>
2025-07-12 08:44:22 +0100

    binary-tree-inorder-traversal

--- src/data.js
+++ src/data.js
@@ -947,6 +947,49 @@ lRUCache.get(4);    // 返回 4
     `,
     "link": "https://leetcode.cn/problems/lru-cache/description/?envType=study-plan-v2&envId=top-100-liked"
   },
+  {
+    "id": 94,
+    "title": "二叉树的中序遍历 binary-tree-inorder-traversal",
+    "category": "二叉树",
+    "content": `
+给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。
+
+
+示例 1：
+
+
+输入：root = [1,null,2,3]
+输出：[1,3,2]
+
+示例 2：
+
+输入：root = []
+输出：[]
+
+示例 3：
+
+输入：root = [1]
+输出：[1]
+ 
+
+提示：
+
+树中节点数目在范围 [0, 100] 内
+-100 <= Node.val <= 100
+ 
+
+进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+    `,
+    "difficulty": "简单",
+    "hint": `
+中序遍历：左子树 - 根节点 - 右子树
+思路一：递归
+    - 从根节点开始，依次递归左右子树
+思路二：迭代
+    - 递归中隐式维护了一个栈，迭代中需要将其模拟出来
+    `,
+    "link": "https://leetcode.cn/problems/binary-tree-inorder-traversal/description/?envType=study-plan-v2&envId=top-100-liked"
+  },
 ]
 
 export default data;
