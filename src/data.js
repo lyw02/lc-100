1c617679a7b5f6b945b03e6e27225cee3b150922
Yiwei Liu <liu02yiwei@gmail.com>
2025-07-12 22:55:20 +0100

    binary-tree-right-side-view

--- src/data.js
+++ src/data.js
@@ -1338,6 +1338,67 @@ nums 按 严格递增 顺序排列
     `,
     link: "https://leetcode.cn/problems/kth-smallest-element-in-a-bst/description/?envType=study-plan-v2&envId=top-100-liked",
   },
+  {
+    id: 199,
+    title: "二叉树的右视图 binary-tree-right-side-view",
+    category: "二叉树",
+    content: `
+给定一个二叉树的 根节点 root，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+
+ 
+
+示例 1：
+
+输入：root = [1,2,3,null,5,null,4]
+
+输出：[1,3,4]
+
+解释：
+
+  1
+2   3
+  5   4
+
+
+示例 2：
+
+输入：root = [1,2,3,4,null,null,null,5]
+
+输出：[1,3,4,5]
+
+解释：
+
+      1
+    2   3
+  4
+5
+
+示例 3：
+
+输入：root = [1,null,3]
+
+输出：[1,3]
+
+示例 4：
+
+输入：root = []
+
+输出：[]
+
+ 
+
+提示:
+
+二叉树的节点个数的范围是 [0,100]
+-100 <= Node.val <= 100 
+    `,
+    difficulty: "中等",
+    hint: `
+- BFS
+- 使用 levelSize 变量记录每层的宽度，当遍历到每层的最后一个时添加到结果
+    `,
+    link: "https://leetcode.cn/problems/binary-tree-right-side-view/description/?envType=study-plan-v2&envId=top-100-liked",
+  },
 ];
 
 export default data;
