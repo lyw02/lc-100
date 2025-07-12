603536a76b1e4eea5a353cf63b2310b293ea68ce
Yiwei Liu <liu02yiwei@gmail.com>
2025-07-12 09:07:45 +0100

    invert-binary-tree

--- src/data.js
+++ src/data.js
@@ -1024,6 +1024,45 @@ lRUCache.get(4);    // 返回 4
     `,
     "link": "https://leetcode.cn/problems/maximum-depth-of-binary-tree/description/?envType=study-plan-v2&envId=top-100-liked"
   },
+  {
+    "id": 226,
+    "title": "翻转二叉树 invert-binary-tree",
+    "category": "二叉树",
+    "content": `
+给你一棵二叉树的根节点 root ，翻转这棵二叉树，并返回其根节点。
+
+
+示例 1：
+
+      4                    4
+  2       7     =>     7       2  
+1   3   6   9        9   6   3   1
+
+输入：root = [4,2,7,1,3,6,9]
+输出：[4,7,2,9,6,3,1]
+
+示例 2：
+
+输入：root = [2,1,3]
+输出：[2,3,1]
+
+示例 3：
+
+输入：root = []
+输出：[]
+ 
+
+提示：
+
+树中节点数目范围在 [0, 100] 内
+-100 <= Node.val <= 100
+    `,
+    "difficulty": "简单",
+    "hint": `
+- 递归左右子树，交换左右子树的位置
+    `,
+    "link": "https://leetcode.cn/problems/invert-binary-tree/description/?envType=study-plan-v2&envId=top-100-liked"
+  },
 ]
 
 export default data;
