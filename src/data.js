f99ca92cf627c831b01e6122943d1c7804528192
Yiwei Liu <liu02yiwei@gmail.com>
2025-07-13 15:43:29 +0100

    lowest-common-ancestor-of-a-binary-tree

--- src/data.js
+++ src/data.js
@@ -1565,6 +1565,69 @@ inorder 保证 为二叉树的中序遍历序列
     `,
     link: "https://leetcode.cn/path-sum-iii/description/?envType=study-plan-v2&envId=top-100-liked",
   },
+  {
+    id: 236,
+    title: "二叉树的最近公共祖先 lowest-common-ancestor-of-a-binary-tree",
+    category: "二叉树",
+    content: `
+给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+
+百度百科中最近公共祖先的定义为：“对于有根树 T 的两个节点 p、q，最近公共祖先表示为一个节点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+
+示例 1：
+
+        3
+  5           1
+6    2      0   8
+   7   4
+
+输入：root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+输出：3
+解释：节点 5 和节点 1 的最近公共祖先是节点 3 。
+
+示例 2：
+
+        3
+  5           1
+6    2      0   8
+   7   4
+
+输入：root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+输出：5
+解释：节点 5 和节点 4 的最近公共祖先是节点 5 。因为根据定义最近公共祖先节点可以为节点本身。
+
+示例 3：
+
+输入：root = [1,2], p = 1, q = 2
+输出：1
+ 
+
+提示：
+
+树中节点数目在范围 [2, 105] 内。
+-109 <= Node.val <= 109
+所有 Node.val 互不相同 。
+p != q
+p 和 q 均存在于给定的二叉树中。
+    `,
+    difficulty: "中等",
+    hint: `
+- LCA 只有三种情况：
+  - p, q 分别位于 root 左右子树中（异侧）
+  - p === root，q 位于 p 的左或右子树中
+  - q === root，p 位于 q 的左或右子树中
+- 递归
+  - 若 root 为 null 或 p 或 q，则返回 root
+  - 递归左右子树，相当于在左右子树中找 p 或 q
+    - 递归返回值可能是：
+      - null：子树中没有找到 p 或 q
+      - p 或 q：子树中找到了 p 或 q
+      - 某个节点：子树中找到了 p 或 q 的 LCA
+  - 若上面左子树的递归结果为 null ，说明左子树没有 p 或 q ，于是返回上面右子树的递归结果。反之亦然
+  - 若上面左子树和右子树的递归结果都不为 null ，说明 p 和 q ，位于左右两侧，直接返回 root
+    `,
+    link: "https://leetcode.cn/lowest-common-ancestor-of-a-binary-tree/description/?envType=study-plan-v2&envId=top-100-liked",
+  },
 ];
 
 export default data;
