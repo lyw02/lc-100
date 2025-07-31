01eb6fc6f8c01a399a1cc0c5bea9351808de5117
Yiwei Liu <liu02yiwei@gmail.com>
2025-07-31 22:50:45 +0100

    jump-game

--- src/data.js
+++ src/data.js
@@ -1681,6 +1681,61 @@ p 和 q 均存在于给定的二叉树中。
     return profit;
 };`
   },
+  {
+    id: 55,
+    title: "跳跃游戏 jump-game",
+    category: "贪心算法",
+    content: `
+给你一个非负整数数组 nums ，你最初位于数组的 第一个下标 。数组中的每个元素代表你在该位置可以跳跃的最大长度。
+
+判断你是否能够到达最后一个下标，如果可以，返回 true ；否则，返回 false 。
+
+ 
+示例 1：
+
+输入：nums = [2,3,1,1,4]
+输出：true
+解释：可以先跳 1 步，从下标 0 到达下标 1, 然后再从下标 1 跳 3 步到达最后一个下标。
+
+示例 2：
+
+输入：nums = [3,2,1,0,4]
+输出：false
+解释：无论怎样，总会到达下标为 3 的位置。但该下标的最大跳跃长度是 0 ， 所以永远不可能到达最后一个下标。
+ 
+
+提示：
+
+1 <= nums.length <= 104
+0 <= nums[i] <= 105
+    `,
+    difficulty: "中等",
+    hint: `
+- 对于每一个可以到达的位置 x，它使得 x + 1, x + 2, ⋯, x + nums[x] 这些连续的位置都可以到达
+- 我们依次遍历数组中的每一个位置，并实时维护最远可以到达的位置
+- 如果 最远可以到达的位置 大于等于数组中的最后一个位置，那就说明最后一个位置可达
+    `,
+    link: "https://leetcode.cn/problems/jump-game/?envType=study-plan-v2&envId=top-100-liked",
+    code: `function canJump(nums: number[]): boolean {
+    // 对于每一个可以到达的位置 x，它使得 x + 1, x + 2, ⋯, x + nums[x] 这些连续的位置都可以到达。
+    // 我们依次遍历数组中的每一个位置，并实时维护最远可以到达的位置。
+    let rightmost = 0;
+    for (let i = 0; i < nums.length; i++) {
+
+        // 如果当前遍历到的位置在 最远可以到达的位置 的范围内，那么我们就可以从起点通过若干次跳跃到达该位置。
+        if (i <= rightmost) {
+            // 可以用 i + nums[i] 更新 最远可以到达的位置。
+            rightmost = Math.max(rightmost, i + nums[i]);
+        }
+
+        // 如果 最远可以到达的位置 大于等于数组中的最后一个位置，那就说明最后一个位置可达。
+        if (rightmost >= nums.length - 1) {
+            return true;
+        }
+    }
+    return false;
+};`
+  },
 ];
 
 export default data;
