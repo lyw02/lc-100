f2237b91812561477a2dfb570741f5d5815a528c
Yiwei Liu <liu02yiwei@gmail.com>
2025-08-01 00:06:49 +0100

    partition-labels

--- src/data.js
+++ src/data.js
@@ -1800,6 +1800,72 @@ i + j < n
     return res;
 };`,
   },
+  {
+    id: 763,
+    title: "划分字母区间 partition-labels",
+    category: "贪心算法",
+    content: `
+给你一个字符串 s 。我们要把这个字符串划分为尽可能多的片段，同一字母最多出现在一个片段中。例如，字符串 "ababcc" 能够被分为 ["abab", "cc"]，但类似 ["aba", "bcc"] 或 ["ab", "ab", "cc"] 的划分是非法的。
+
+注意，划分结果需要满足：将所有划分结果按顺序连接，得到的字符串仍然是 s 。
+
+返回一个表示每个字符串片段的长度的列表。
+
+示例 1：
+
+输入：s = "ababcbacadefegdehijhklij"
+输出：[9,7,8]
+解释：
+划分结果为 "ababcbaca"、"defegde"、"hijhklij" 。
+每个字母最多出现在一个片段中。
+像 "ababcbacadefegde", "hijhklij" 这样的划分是错误的，因为划分的片段数较少。
+
+示例 2：
+
+输入：s = "eccbbbbdec"
+输出：[10]
+
+提示：
+
+1 <= s.length <= 500
+s 仅由小写英文字母组成
+    `,
+    difficulty: "中等",
+    hint: `
+- 由于同一个字母只能出现在同一个片段，显然同一个字母的第一次出现的下标位置和最后一次出现的下标位置必须出现在同一个片段
+- 因此需要遍历字符串，得到每个字母最后一次出现的下标位置
+- 可以使用哈希表记录每个字母最后一次出现的位置
+- 使用两个变量记录当前片段的起始位置 start 和结束位置 end
+- 遍历到位置 i 时，使用该位置的字母最后一次出现的位置，即 max(end, map.get(s[i])) 更新 end
+- 当遍历到 end 位置时，开始下一个片段
+    `,
+    link: "https://leetcode.cn/problems/partition-labels/?envType=study-plan-v2&envId=top-100-liked",
+    code: `function partitionLabels(s: string): number[] {
+    const res = [];
+
+    // 遍历字符串，得到每个字母最后一次出现的下标位置
+    const map = new Map();
+    for (let i = 0; i < s.length; i++) {
+        map.set(s[i], i);
+    }
+
+    // 当前片段的开始下标 start 和结束下标 end
+    let start = 0;
+    let end = 0;
+    for (let i = 0; i < s.length; i++) {
+        // 使用当前字母的最后一次出现的下标位置更新 end
+        end = Math.max(end, map.get(s[i]));
+
+        // 当前片段访问结束，继续寻找下一个片段
+        if (i === end) {
+            res.push(end - start + 1);
+            start = end + 1;
+        }
+    }
+
+    return res;
+};`,
+  },
 ];
 
 export default data;
