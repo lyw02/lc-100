3aa121c5440c0059416c677fb4ec2be00da2a772
Yiwei Liu <liu02yiwei@gmail.com>
2025-08-01 22:53:08 +0100

    group-anagrams

--- src/data.js
+++ src/data.js
@@ -53,6 +53,52 @@ const data = [
 };`
   },
   {
+    id: 49,
+    title: "字母异位词分组 group-anagrams",
+    category: "哈希",
+    content: `
+给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
+
+示例 1:
+
+输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+
+输出: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+解释：
+
+在 strs 中没有字符串可以通过重新排列来形成 "bat"。
+字符串 "nat" 和 "tan" 是字母异位词，因为它们可以重新排列以形成彼此。
+字符串 "ate" ，"eat" 和 "tea" 是字母异位词，因为它们可以重新排列以形成彼此。
+
+示例 2:
+
+输入: strs = [""]
+
+输出: [[""]]
+
+示例 3:
+
+输入: strs = ["a"]
+
+输出: [["a"]]
+
+提示：
+
+1 <= strs.length <= 104
+0 <= strs[i].length <= 100
+strs[i] 仅包含小写字母
+    `,
+    difficulty: "中等",
+    hint: `
+- 由于互为字母异位词的两个字符串包含的字母相同，因此两个字符串中的相同字母出现的次数一定是相同的
+- 故可以将每个字母出现的次数使用字符串表示，作为哈希表的键
+- 由于字符串只包含小写字母，因此对于每个字符串，可以使用长度为 26 的数组记录每个字母出现的次数
+`,
+    link: "https://leetcode.cn/problems/group-anagrams/description/?envType=study-plan-v2&envId=top-100-liked",
+    code: ``
+  },
+  {
     id: 189,
     title: "轮转数组 rotate-array",
     category: "普通数组",
