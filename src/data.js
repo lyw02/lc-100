95acbc6f627dd0387ac1bbc3d0caa5942748234f
Yiwei Liu <liu02yiwei@gmail.com>
2025-08-01 17:38:09 +0100

    single-number

--- src/data.js
+++ src/data.js
@@ -3007,6 +3007,51 @@ word1 和 word2 由小写英文字母组成
     return dp[m][n];
 };`,
   },
+  {
+    id: 136,
+    title: "只出现一次的数字 single-number",
+    category: "技巧",
+    content: `
+给你一个 非空 整数数组 nums ，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+
+你必须设计并实现线性时间复杂度的算法来解决此问题，且该算法只使用常量额外空间。
+
+
+示例 1 ：
+
+输入：nums = [2,2,1]
+
+输出：1
+
+示例 2 ：
+
+输入：nums = [4,1,2,1,2]
+
+输出：4
+
+示例 3 ：
+
+输入：nums = [1]
+
+输出：1
+
+提示：
+
+1 <= nums.length <= 3 * 104
+-3 * 104 <= nums[i] <= 3 * 104
+除了某个元素只出现一次以外，其余每个元素均出现两次。
+    `,
+    difficulty: "简单",
+    hint: `
+- 要求使用常量额外空间，因此考虑位运算
+- 异或运算有个重要的性质，两个相同数字异或为 0
+- 因此只需要将所有元素异或，剩下的就是只出现一次的元素
+    `,
+    link: "https://leetcode.cn/problems/single-number/?envType=study-plan-v2&envId=top-100-liked",
+    code: `function singleNumber(nums: number[]): number {
+    return nums.reduce((acc, cur) => acc ^ cur, 0);
+};`,
+  },
 ];
 
 export default data;
