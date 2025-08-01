7238de9a7f02d2fcebaf1239c3005eeaa9a2f357
Yiwei Liu <liu02yiwei@gmail.com>
2025-08-01 23:30:15 +0100

    container-with-most-water

--- src/data.js
+++ src/data.js
@@ -224,6 +224,57 @@ function moveZeroes(nums: number[]): void {
 };`
   },
   {
+    id: 11,
+    title: "盛最多水的容器 container-with-most-water",
+    category: "双指针",
+    content: `
+给定一个长度为 n 的整数数组 height 。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i]) 。
+
+找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+
+返回容器可以储存的最大水量。
+
+说明：你不能倾斜容器。
+
+示例 1：
+
+输入：[1,8,6,2,5,4,8,3,7]
+输出：49 
+解释：图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水（表示为蓝色部分）的最大值为 49。
+
+示例 2：
+
+输入：height = [1,1]
+输出：1
+
+提示：
+
+n == height.length
+2 <= n <= 105
+0 <= height[i] <= 104
+    `,
+    difficulty: "中等",
+    hint: `
+- 双指针
+- 左右两个指针分别指向左右边界
+- 可容纳的水量相当于面积：S(i, j) = min(h[i], h[j]) * (j - i)
+- 如果向内移动短板，面积可能变大
+- 如果向内移动长板，面积一定变小
+- 因此每次将短板向内移动，更新全局最大面积，知道两指针相遇
+`,
+    link: "https://leetcode.cn/problems/container-with-most-water/description/?envType=study-plan-v2&envId=top-100-liked",
+    code: `function maxArea(height: number[]): number {
+    let i = 0;
+    let j = height.length - 1;
+    let maxArea = 0;
+    while (i < j) {
+        maxArea = Math.max(maxArea, Math.min(height[i], height[j]) * (j - i))
+        height[i] < height[j] ? i++ : j--;
+    }
+    return maxArea;
+};`
+  },
+  {
     id: 189,
     title: "轮转数组 rotate-array",
     category: "普通数组",
