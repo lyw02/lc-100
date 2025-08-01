ed8e5a66c41be4f2ebc7a98d653b6281720cb01b
Yiwei Liu <liu02yiwei@gmail.com>
2025-08-01 22:29:56 +0100

    two-sum

--- src/data.js
+++ src/data.js
@@ -1,5 +1,58 @@
 const data = [
   {
+    id: 1,
+    title: "两数之和 two-sum",
+    category: "哈希",
+    content: `
+给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target 的那 两个 整数，并返回它们的数组下标。
+
+你可以假设每种输入只会对应一个答案，并且你不能使用两次相同的元素。
+
+你可以按任意顺序返回答案。
+
+示例 1：
+
+输入：nums = [2,7,11,15], target = 9
+输出：[0,1]
+解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+
+示例 2：
+
+输入：nums = [3,2,4], target = 6
+输出：[1,2]
+
+示例 3：
+
+输入：nums = [3,3], target = 6
+输出：[0,1]
+
+提示：
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+只会存在一个有效答案
+
+进阶：你可以想出一个时间复杂度小于 O(n^2) 的算法吗？
+    `,
+    difficulty: "简单",
+    hint: `
+- 目标是要找到两个数：x 和 target - x
+- 创建一个哈希表，对于每一个 x，我们首先查询哈希表中是否存在 target - x，然后将 x 插入到哈希表中，键为 x，值为下标
+`,
+    link: "https://leetcode.cn/problems/two-sum/description/?envType=study-plan-v2&envId=top-100-liked",
+    code: `function twoSum(nums: number[], target: number): number[] {
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        if (map.has(target - nums[i])) {
+            return [map.get(target - nums[i]), i];
+        } else {
+            map.set(nums[i], i);
+        }
+    }
+};`
+  },
+  {
     id: 189,
     title: "轮转数组 rotate-array",
     category: "普通数组",
