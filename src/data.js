5d974c1f29247881f9011bed76114c6287fc42cf
Yiwei Liu <liu02yiwei@gmail.com>
2025-08-12 03:34:18 +0100

    merge-two-sorted-lists

--- src/data.js
+++ src/data.js
@@ -2109,8 +2109,6 @@ function detectCycle(head: ListNode | null): ListNode | null {
     content: `
 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
 
- 
-
 示例 1：
 
 输入：l1 = [1,2,4], l2 = [1,3,4]
@@ -2125,7 +2123,6 @@ function detectCycle(head: ListNode | null): ListNode | null {
 
 输入：l1 = [], l2 = [0]
 输出：[0]
- 
 
 提示：
 
@@ -2139,6 +2136,39 @@ l1 和 l2 均按 非递减顺序 排列
 - 注意两链表长度不同的情况
 `,
     link: "https://leetcode.cn/problems/merge-two-sorted-lists/description/?envType=study-plan-v2&envId=top-100-liked",
+    code: `/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+    const dummyHead = new ListNode(-Infinity);
+
+    let prev = dummyHead;
+
+    while (list1 && list2) {
+        if (list1.val <= list2.val) {
+            prev.next = list1;
+            list1 = list1.next;
+        } else {
+            prev.next = list2;
+            list2 = list2.next
+        }
+        prev = prev.next;
+    }
+
+    prev.next = list1 ?? list2;
+
+    return dummyHead.next;
+};
+`,
   },
   {
     id: 2,
