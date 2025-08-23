e5033bf641150123291b3a7d2eecd81591f6da1a
Yiwei Liu <liu02yiwei@gmail.com>
2025-08-23 17:33:00 +0800

    search-insert-position

--- src/data.js
+++ src/data.js
@@ -4139,7 +4139,7 @@ n 皇后问题 研究的是如何将 n 个皇后放置在 n×n 的棋盘上，�
 
 1 <= n <= 9
     `,
-    difficulty: "中等",
+    difficulty: "困难",
     hint: `
 - 回溯
 - 每一行必须且只能放置一个皇后。如果我们在一行里不放皇后，那么就不可能放满 N 个。如果在一行里放两个，她们会立即互相攻击
@@ -4207,6 +4207,58 @@ n 皇后问题 研究的是如何将 n 个皇后放置在 n×n 的棋盘上，�
 
     backtrack(0);
     return res;
+};`,
+  },
+  {
+    id: 35,
+    title: "搜索插入位置 search-insert-position",
+    category: "二分查找",
+    content: `
+给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+
+请必须使用时间复杂度为 O(log n) 的算法。
+
+示例 1:
+
+输入: nums = [1,3,5,6], target = 5
+输出: 2
+
+示例 2:
+
+输入: nums = [1,3,5,6], target = 2
+输出: 1
+
+示例 3:
+
+输入: nums = [1,3,5,6], target = 7
+输出: 4
+
+提示:
+
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums 为 无重复元素 的 升序 排列数组
+-104 <= target <= 104
+    `,
+    difficulty: "简单",
+    hint: `
+- 二分查找
+    `,
+    link: "https://leetcode.cn/search-insert-position/description/?envType=study-plan-v2&envId=top-100-liked",
+    code: `function searchInsert(nums: number[], target: number): number {
+    let i = 0;
+    let j = nums.length - 1;
+    while (i <= j) {
+        const m = Math.floor((i + j) / 2);
+        if (nums[m] === target) {
+            return m;
+        } else if (nums[m] < target) {
+            i = m + 1;
+        } else {
+            j = m - 1;
+        }
+    }
+    return i;
 };`,
   },
   {
