d16631b99c715f678649e576e457cc7ea12c7417
Yiwei Liu <liu02yiwei@gmail.com>
2025-09-02 17:12:30 +0800

    binary-tree-inorder-traversal

--- src/data.js
+++ src/data.js
@@ -2879,9 +2879,7 @@ class LRUCache {
     content: `
 给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。
 
-
 示例 1：
-
 
 输入：root = [1,null,2,3]
 输出：[1,3,2]
@@ -2895,13 +2893,11 @@ class LRUCache {
 
 输入：root = [1]
 输出：[1]
- 
 
 提示：
 
 树中节点数目在范围 [0, 100] 内
 -100 <= Node.val <= 100
- 
 
 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
     `,
@@ -2914,6 +2910,51 @@ class LRUCache {
     - 递归中隐式维护了一个栈，迭代中需要将其模拟出来
     `,
     link: "https://leetcode.cn/problems/binary-tree-inorder-traversal/description/?envType=study-plan-v2&envId=top-100-liked",
+    code: `/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+// 递归
+function inorderTraversal(root: TreeNode | null): number[] {
+    if (!root) return [];
+    const res = [];
+    dfs(root, res);
+    return res;
+};
+
+function dfs(root: TreeNode | null, res: number[]): void {
+    if (!root) return;
+    dfs(root.left, res);
+    res.push(root.val);
+    dfs(root.right, res);
+}
+
+// 迭代
+function inorderTraversal(root: TreeNode | null): number[] {
+    if (!root) return [];
+    const res = [];
+    const stack = [];
+    while (root || stack.length) {
+        while (root) {
+            stack.push(root);
+            root = root.left;
+        }
+        root = stack.pop();
+        res.push(root.val);
+        root = root.right;
+    }
+    return res;
+};`
   },
   {
     id: 104,
