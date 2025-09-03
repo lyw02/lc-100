b425674d40698be7dddb9dd3b35014afc13b5560
Yiwei Liu <liu02yiwei@gmail.com>
2025-09-03 12:27:36 +0800

    convert-sorted-array-to-binary-search-tree

--- src/data.js
+++ src/data.js
@@ -3365,11 +3365,9 @@ function levelOrder(root: TreeNode | null): number[][] {
   1  或  1
 3          3
 
-
 输入：nums = [1,3]
 输出：[3,1]
 解释：[1,null,3] 和 [3,1] 都是高度平衡二叉搜索树。
- 
 
 提示：
 
@@ -3386,6 +3384,30 @@ nums 按 严格递增 顺序排列
 - 递归创建左右子树
     `,
     link: "https://leetcode.cn/problems/convert-sorted-array-to-binary-search-tree/description/?envType=study-plan-v2&envId=top-100-liked",
+    code: `/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function sortedArrayToBST(nums: number[]): TreeNode | null {
+    return dfs(nums, 0, nums.length);
+};
+
+function dfs(nums: number[], left: number, right: number) {
+    if (left === right) return null;
+    const m = Math.floor((left + right) / 2);
+    return new TreeNode(nums[m], dfs(nums, left, m), dfs(nums, m + 1, right));
+}
+`,
   },
   {
     id: 98,
